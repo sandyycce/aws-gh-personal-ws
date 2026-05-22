ab1db6425ee101f6fa80bc6b2648d72b3464b412
Sandip <sandyycce14@gmail.com>
2026-05-22 20:02:11 +0530

    uploading to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,35 @@
         bucket = var.bucket_name
       
     }
+
+    resource "aws_s3_bucket_website_configuration" "stattic_website_config" {
+        bucket = aws_s3_bucket.static-site.id
+        index_document {
+            suffix = "index.html"
+        }
+ 
+    }
+
+    resource "aws_s3_bucket_policy" "bucket_policy" {
+        bucket = aws_s3_bucket.static-site.id
+        policy = jsonencode({
+            Version = "2012-10-17"
+            Statement = [
+                {
+                    Effect = "Allow"
+                    Principal = "*"
+                    Action = "s3:GetObject"
+                    Resource = "${aws_s3_bucket.static-site.arn}/*"
+                }
+            ]
+        })
+    }
+
+    resource "aws_s3_bucket_public_access_block" "sttaic_site_access_block" {
+        bucket = aws_s3_bucket.static-site.id
+        block_public_acls = false
+        block_public_policy = false
+        ignore_public_acls = false
+        restrict_public_buckets = false
+      
+    }
