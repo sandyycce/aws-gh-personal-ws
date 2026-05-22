9bfde81e33671781329fafe9845cb4d0f47915d3
Sandip <sandyycce14@gmail.com>
2026-05-22 20:12:08 +0530

    uploading to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,15 @@
  
     }
 
+        resource "aws_s3_bucket_public_access_block" "sttaic_site_access_block" {
+        bucket = aws_s3_bucket.static-site.id
+        block_public_acls = false
+        block_public_policy = false
+        ignore_public_acls = false
+        restrict_public_buckets = false
+      
+    }
+
     resource "aws_s3_bucket_policy" "bucket_policy" {
         bucket = aws_s3_bucket.static-site.id
         policy = jsonencode({
@@ -24,13 +33,7 @@
                 }
             ]
         })
-    }
 
-    resource "aws_s3_bucket_public_access_block" "sttaic_site_access_block" {
-        bucket = aws_s3_bucket.static-site.id
-        block_public_acls = false
-        block_public_policy = false
-        ignore_public_acls = false
-        restrict_public_buckets = false
-      
-    }
+        depends_on = [ aws_s3_bucket_public_access_block.sttaic_site_access_block ]
+    }   
+
